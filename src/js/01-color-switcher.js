13dbd6d7577670d979f0cf4f8e610abd94eb6c90
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-06-14 16:05:24 +0300

    123

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function handleStartBtnClick() {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeBgColor, 1000);
+}
+
+function handleStopBtnClick() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', handleStartBtnClick);
+stopBtn.addEventListener('click', handleStopBtnClick);
