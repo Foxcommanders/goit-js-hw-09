7e93e2731de9364a5012ddb302fbcc923d3d242d
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-06-16 18:51:42 +0000

    Deploying to gh-pages from @ Foxcommanders/goit-js-hw-09@afa6ea79368a727f6c81c99a90918dcac91b763a 🚀

--- 01-color-switcher.46c9e5dd.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;function c(){n.style.backgroundColor=getRandomHexColor()}e.addEventListener("click",(function(){e.disabled=!0,o=setInterval(c,1e3)})),t.addEventListener("click",(function(){e.disabled=!1,clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.46c9e5dd.js.map
